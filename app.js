8db9cbc132a2b28e9246c636a7074bdf9c2f443a
Dharmesh <sdharms@amazon.com>
2017-10-10 01:11:42 +0530

    Added basic route handling for /

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const app = express();
 
+app.get("/", function(req, res) {
+	res.send('Rick Sanchez we know is not the C-137 Morty\'s Rick :P')
+});
+
 
 module.exports = app;
