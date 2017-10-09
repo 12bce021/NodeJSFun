83a6a8b33e08e53ba4f47c32d3e4d60e05dee29e
Dharmesh <sdharms@amazon.com>
2017-10-10 03:08:53 +0530

    Error handling and landing page

--- app.js
+++ app.js
@@ -1,9 +1,39 @@
-const express = require('express');
-const app = express();
+var express = require('express');
+var app = express();
 
 app.get("/", function(req, res) {
-	res.send('Rick Sanchez we know is not the C-137 Morty\'s Rick :P')
+	res.render('index.html')
 });
 
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('404.html', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('404.html', {
+    message: err.message,
+    error: {}
+  });
+});
 
 module.exports = app;
